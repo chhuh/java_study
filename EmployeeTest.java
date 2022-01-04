cd5320b6e55ce8a62fa17236d448b6ff2560bc03
chhuh <huh1125@ajou.ac.kr>
2022-01-04 11:42:47 +0900

    new code

---
+++ EmployeeTest.java
@@ -0,0 +1,9 @@
+package employeeProject;
+
+public class EmployeeTest {
+	public static void main(String[] args) {
+		Employee huh;
+		huh = new Employee("huh", 1125, 26);
+		System.out.println(huh.toString());
+	}
+}
